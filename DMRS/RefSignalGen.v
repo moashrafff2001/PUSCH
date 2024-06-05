5a7abe9455a5dd68e474e2201e5c8d04f5fdae7e
KarimIbrahim25 <karim25401@gmail.com>
2024-06-05 22:42:16 +0300

    Update RefSignalGen.v

--- DMRS/RefSignalGen.v
+++ DMRS/RefSignalGen.v
@@ -14,17 +14,18 @@ module RefSignalGen (
 	input signed [8:0] sin_value,
 	input signed [8:0] cos_value,
 	output reg [9:0] counter,
-	output reg [12:0] phase,
+	output reg [14:0] phase,
 	output wire signed [8:0] DMRS_r,
 	output wire signed [8:0] DMRS_i,
 	output reg DMRS_valid
 );
 
 // Step Calculations for zadoff-chu sequence
-reg [24:0] step;
-reg [12:0] phase_in;
+reg [25:0] step;
 reg [43:0] step_first;
-reg [24:0] step_init;
+reg [25:0] step_init;
+reg [25:0] step_next;
+reg [14:0] phase_next;
 
 // For q and Nzc Calculations
 reg [34:0] mult; 
@@ -35,8 +36,8 @@ reg [9:0] Nzc;
 reg [33:0] Nzc_rec;
 
 // Finished signal
-wire finished;
-assign finished = (counter == Mzc) ? 1 : 0;
+reg finished;
+//assign finished = (counter == Mzc) ? 1 : 0;
 
 // Outputs
 assign DMRS_r = cos_value;
@@ -67,15 +68,15 @@ always@(*)
 		//  Calculating the initial step for zadoff-chu sequence
 		if (Mzc >= 'd36)
 			begin
-				step_first = q * Nzc_rec; // Q10.0*Q0.34 = Q10.34 -> Q0.25
-				step_init = step_first[33:09] + step_first[8]; // rounding the first step
+				step_first = q * Nzc_rec; // Q10.0*Q0.34 = Q10.34 -> Q0.26
+				step_init = step_first[33:08] + step_first[7]; // rounding the first step
 			end
 		
 		//  Calculating the initial step for Mzc = 30		
 		else
 			begin
 				step_first = 44'b0;
-				step_init = {step_init[19:0], 5'b00000}; // Q5.20 -> Q0.20 + Q0.5 -> Q0.25
+				step_init = {step_init[19:0], 6'b000000}; // Q5.20 -> Q0.20 + Q0.5 -> Q0.25
 			end
 			
 	end
@@ -86,83 +87,98 @@ always@(posedge clk or negedge reset)
 		if (!reset)
 			begin
 				DMRS_valid <= 1'b0;
-				phase <= 13'b0;
 				counter <= 10'b0;
+				step <= 26'b0;
+        		phase <= 15'b0;
+				finished <= 1'b0;
 			end
-			
+		
+		else if (counter == Mzc)
+			begin
+				DMRS_valid <= 1'b0;
+				counter <= 10'b0;
+				step <= 26'b0;
+        		phase <= 15'b0;
+				finished <= 1'b1;
+			end
+
 		else if (enable && (!finished))
 			begin
 				DMRS_valid <= 1'b1;
-				phase <= phase_in;
 				counter <= counter + 1;
+				step <= step_next;
+        		phase <= phase_next;
+				finished <= 1'b0;
 			end
 			
 		else
 			begin
 				DMRS_valid <= 1'b0;
-				phase <= 13'b0;
 				counter <= 10'b0;
+				step <= 26'b0;
+        		phase <= 15'b0;
+				finished <= 1'b0;
 			end
 		end
 		
 // DM-RS Calculations
 always@(*)
 	begin
-		if (Mzc >= 'd36)
-			begin
-				if ((counter == 0) || (counter == Nzc))
-					begin
-						step = 25'b0;
-						phase_in = 13'b0;
-					end
-					
-				else begin
-					step = step + step_init; // Q0.25+Q0.25 = Q0.26 -> rounding to Q0.25
-					phase_in = phase_in + step[24:12] + step[11];// phase_in rounding
-				end
-			end
-		
-		else if (Mzc == 'd30)
-			begin
-				if (counter == 0)
-					begin
-						step = step_init;
-						phase_in = step[24:12] + step[11];
-					end
-					
-				else begin
-					step = step + step_init; // Q0.25+Q0.25 = Q0.26 -> rounding to Q0.25
-					phase_in = phase_in + step[24:12] + step[11];// phase_in rounding
-				end
-			end
+		// Assign the next state values
+		step_next = step;
+    	phase_next = phase;
+
+		if (Mzc >= 'd36) begin
+			if ((counter == 0) || (counter == Nzc)) begin
+				step_next = 26'b0;
+				phase_next = 15'b0;
+			end 
 			
-		else
-			begin
-				step = 25'b0;
-				case (Mzc)
-					6		: phase_in = {phi1_value, 11'b10000000000};
-
-					12		: phase_in = {phi2_value, 11'b10000000000};
-
-					18		: phase_in = {phi3_value, 11'b10000000000};
-
-					24		: phase_in = {phi4_value, 11'b10000000000};
-					
-					default : phase_in = 13'b0; // In case of unavailable sequence length
-				endcase
-				
-				// In case of +ve phi the shift already does not matter since there are 2 integer bits
-				// and 1 sign bit which equals to zero
-				
-				// In case of -ve phi, we need to calculate the 2s complement to wrap the -ve value
-				if (phase_in[12])
-					begin
-						phase_in[11:0] = phase_in[11:0] >> 1; // Correct value of phase_in
-						phase_in = ~phase_in + 1'b1; // Twos complement to get the -ve values correct
-						phase_in = phase_in << 1; // Get rid of sign bit
-					end
-
+			else begin
+				step_next = step + step_init; // Q0.26+Q0.26 = Q0.27 -> rounding to Q0.26
+				phase_next = phase + step_next[25:11] + step_next[10]; // phase rounding
 			end
+		end 
+
+		else if (Mzc == 'd30) 
+		begin
+			if (counter == 0) begin
+				step_next = step_init;
+				phase_next = step_next[25:11] + step_next[10];
+			end 
+			
+			else begin
+				step_next = step + step_init; // Q0.26+Q0.26 = Q0.27 -> rounding to Q0.26
+				phase_next = phase + step_next[25:11] + step_next[10]; // phase rounding
+			end
+		end 
+		
+		else begin
+			step_next = 26'b0;
+			case (Mzc)
+				6   : phase_next = {phi1_value, 13'b1000000000000};
+
+				12  : phase_next = {phi2_value, 13'b1000000000000};
+
+				18  : phase_next = {phi3_value, 13'b1000000000000};
+
+				24  : phase_next = {phi4_value, 13'b1000000000000};
+
+				default : phase_next = 13'b0; // In case of unavailable sequence length
+			endcase
+			
+			// In case of +ve phi the shift already does not matter since there are 2 integer bits
+			// and 1 sign bit which equals to zero
+			
+			// In case of -ve phi, we need to calculate the 2s complement to wrap the -ve value
+			if (phase_next[14]) begin
+				phase_next[13:0] = phase_next[13:0] >> 1; // Correct value of phase
+				phase_next = ~phase_next + 1'b1; // Twos complement to get the -ve values correct
+				phase_next = phase_next << 1; // Get rid of sign bit
+			end
+		end
+
+		
 
 	end
 endmodule
