1618d11b02b85b1e46735d00381d86f99b9a70a4
KarimIbrahim25 <karim25401@gmail.com>
2024-06-05 22:42:53 +0300

    Update TopDMRS.v

--- DMRS/TopDMRS.v
+++ DMRS/TopDMRS.v
@@ -2,21 +2,23 @@ module TopDMRS (
 	input clk,
 	input reset,
 	input enable,
+	input [3:0] N_slot_frame,
 	input [9:0] n_ID,
 	input [6:0] N_rb,
 	input [1:0] En_hopping,
-	input [7:0] c,
+	input c,
 	output wire signed [8:0] DMRS_r,
 	output wire signed [8:0] DMRS_i,
 	output wire DMRS_valid
 );
 
-wire [12:0] phase;
+wire [14:0] phase;
 wire signed [8:0] sin_value, cos_value;
 wire [9:0] Mzc;
 wire [9:0] counter;
 wire [4:0] u;
 wire v;
+wire enable_gen;
 wire [9:0] Nzc;
 wire [29:0] Nzc_rec;
 wire [1:0] phi1_value;
@@ -25,11 +27,11 @@ wire [1:0] phi3_value;
 wire [1:0] phi4_value;
 
 // Reference Signal Generator
-RefSignalGen Dmrs_Generator (clk, reset, enable, Mzc, u, v, Nzc, Nzc_rec, phi1_value, phi2_value, phi3_value,
+RefSignalGen Dmrs_Generator (clk, reset, enable_gen, Mzc, u, v, Nzc, Nzc_rec, phi1_value, phi2_value, phi3_value,
 							 phi4_value, sin_value, cos_value, counter, phase, DMRS_r, DMRS_i, DMRS_valid);
 
 // Paramters Generator
-ParamGen Dmrs_ParamGenerator (n_ID, N_rb, En_hopping, c, Mzc, u, v);
+ParamGen Dmrs_ParamGenerator (clk, reset, enable, N_slot_frame, n_ID, N_rb, En_hopping, c, Mzc, u, v, enable_gen);
 
 // Sine LUT
 SinLUT Dmrs_LookupTable (phase, sin_value, cos_value);
