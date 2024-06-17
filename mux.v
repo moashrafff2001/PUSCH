ce2c110325199003e266b85fa89f6b7b0c7d5d8c
KarimIbrahim25 <karim25401@gmail.com>
2024-06-17 23:35:25 +0300

    Delete mux.v

--- mux.v
+++
@@ -1,8 +0,0 @@
-module mux #(parameter WIDTH = 18) (
-    input wire [WIDTH-1:0] input_0,
-    input wire [WIDTH-1:0] input_1,
-    input wire select,
-    output wire [WIDTH-1:0] out
-);
-    assign out = select ? input_1 : input_0;
-endmodule
